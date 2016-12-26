6131fd8a9c00a806c9fb3fdd9270c0bd413262c6
Peter Tempfli <tempflip@gmail.com>
2016-12-26 19:55:07 +0100

    hough

--- hough_transform.py
+++ hough_transform.py
@@ -18,19 +18,21 @@ def add_to_fetaure_space_linear(p, feature_space, M_MAX = 20, B_COEFF = 0.1):
 
 		b = int((y - m * x) * B_COEFF) 
 
-		#print "i", i, "m", m, "b", b
 
 		if b > vmax or b < vmin : continue
 		b+= vmax-1
 
-		print "############i", i, "m", m, "b", b
 
 		feature_space[i, b] += 1
 
 	return feature_space
 
-def space_point_to_line(p, M_MAX = 20, B_COEFF = 0.1):
-	pass
+def space_point_to_line(p, feature_space, M_MAX = 20, B_COEFF = 0.1):
+	space = np.linspace(- M_MAX, M_MAX, len(feature_space))
+	m = space[p[0]]
+	b = p[1] - len(feature_space[0]) / 2
+
+	return m, b
 
 def get_coordinates(feature_space, val):
 	points = []
@@ -41,39 +43,31 @@ def get_coordinates(feature_space, val):
 		points.append((coords[0][i], coords[1][i]))
 	return points
 
-"""
-def add_to_fetaure_space(p, feature_space):
+def build_line(shape, m, b, val = 99):
+	im = np.zeros(shape)
+	for x in range(shape[1]):
+		point_y = int(m * x + b)
+		if point_y < 0 or point_y >= shape[0] : continue
 
-	N = 10
-
-	d = sqrt(p[0] ** 2 + p[1] ** 2)
-	#print "d", d
-
-	for i in range(len(feature_space)-1):
-		d = i+1 + 0.0
-		sin_p = p[0] / d
-		cos_p = p[1] / d
-
-		if sin_p > 1 or cos_p > 1 : continue 
-
-		#print "## theta feature at d = {}, sin_p = {}, cost_p = {}".format(d, sin_p, cos_p)
-		theta = asin(sin_p)
-
-		feature_value = int(theta * 40)
-		#print "####### theta value : {} at point d {}".format(theta, d)
-
-		feature_space[d / N, feature_value] = feature_space[d / N, feature_value] + 1
-	return feature_space
-"""
+		im[point_y][x] = val
+	return im
 
 
-M_MAX = 20
-B_COEFF = 0.1
+M_MAX = 2
+B_COEFF = 1
 
 im = np.zeros((100, 100))
 feature_space = np.zeros((200, 200)) # d, theta
 
 points = [(10, 10), (20, 20), (30,30), (40, 40), (50, 50), (40, 32), (12, 76), (5, 5), (50, 10), (55, 15), (60, 20), (70, 30)]
+
+points1 = [(10, 10), (20, 20), (30,30), (40, 40)]
+points2 = [(10, 10), (15, 20), (20,30), (25, 40)]
+points3 = [(50, 10), (60, 20), (70,30), (80, 40)]
+
+points4 = [(1,1), (10, 50)]
+
+
 
 #points = [(10, 40), (50, 77), (33, 80), (40, 75), (90, 70)]
 
@@ -89,21 +83,25 @@ val_set = list(set(feature_space.flatten()))
 print val_set
 
 candidates = get_coordinates(feature_space, val_set[-1])
+
+proposal = np.array(im)
+
 for p in candidates:
-	print feature_space[p[0], p[1]]
+	print "candidate:", p
+	m, b = space_point_to_line(p, feature_space, M_MAX = M_MAX, B_COEFF = B_COEFF)
 
-	print space_point_to_line(p, M_MAX = M_MAX, B_COEFF = B_COEFF)
-
-#exit()
-
+	line = build_line(proposal.shape, m, b, val = 3)
+	proposal += line
 
 
-plt.subplot(1,2,1)
+
+
+plt.subplot(1,3,1)
 plt.imshow(im, interpolation="none")
-plt.subplot(1,2,2)
+plt.subplot(1,3,2)
 plt.imshow(feature_space.T, interpolation="none")
-#plt.subplot(3,1,3)
-#plt.imshow(best_matches.T, interpolation="none")
+plt.subplot(1,3,3)
+plt.imshow(proposal, interpolation="none")
 
 
 plt.show()
